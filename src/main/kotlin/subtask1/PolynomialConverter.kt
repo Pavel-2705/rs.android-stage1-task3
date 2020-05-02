30a3dad9a56263ab926b9aaaccd206b33846410b
Kozel_Pavel <pavelshherba@gmail.com>
2020-05-02 18:19:40 +0300

    feat: add subtask1 PolynomialConverter

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -2,8 +2,35 @@ package subtask1
 
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        return if (numbers.isEmpty()) null else {
+            val outString = StringBuilder()
+            outString.append(numbers[0])
+            addDegreeToStringBuilder(numbers.size - 1, outString)
+            for (index in 1 until numbers.size) {
+                val degree = numbers.size - index - 1
+                var number = numbers[index]
+                if (number != 0) {
+                    if (number < 0) {
+                        outString.append(" - ")
+                        number *= -1
+                    } else if (number > 0) {
+                        outString.append(" + ")
+                    }
+                    if (number != 1) outString.append(number)
+                    addDegreeToStringBuilder(degree, outString)
+                }
+            }
+            outString.toString()
+        }
     }
 }
+
+fun addDegreeToStringBuilder(degree: Int, stringBuilder: java.lang.StringBuilder) {
+    when (degree) {
+        0 -> stringBuilder.append("")
+        1 -> stringBuilder.append("x")
+        else -> stringBuilder.append("x^${degree}")
+    }
+}
+
