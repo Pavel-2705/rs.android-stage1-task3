741bcf8b83dee0fd118555ef03f96ff0d13b82b6
Kozel_Pavel <pavelshherba@gmail.com>
2020-05-02 22:05:37 +0300

    feat: add subtask3 - ArrayCalculator

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -2,8 +2,32 @@ package subtask3
 
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        val filteredIntArray = itemsFromArray.filterIsInstance<Int>().toMutableList()
+
+        if (filteredIntArray.isEmpty()) return 0
+        if (numberOfItems >= filteredIntArray.size) return filteredIntArray.reduce { acc, i -> acc*i }
+
+        filteredIntArray.sortDescending()
+        var product = 1
+        if (numberOfItems % 2 == 1) {
+            product *= filteredIntArray.first()
+            filteredIntArray.removeAt(0)
+        }
+        for (i in 1..numberOfItems / 2) {
+            val leftPair = filteredIntArray.first() * filteredIntArray[1]
+            val rightPair =
+                filteredIntArray[filteredIntArray.size - 1] * filteredIntArray[filteredIntArray.size - 2]
+            if (leftPair >= rightPair) {
+                product *= leftPair
+                filteredIntArray.removeAt(1)
+                filteredIntArray.removeAt(0)
+            } else {
+                product *= rightPair
+                filteredIntArray.removeAt(filteredIntArray.size - 2)
+                filteredIntArray.removeAt(filteredIntArray.size - 1)
+            }
+        }
+        return product
     }
 }
